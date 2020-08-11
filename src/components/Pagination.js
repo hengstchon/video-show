b3a5ac077858447560f0958e91c3f7f0c88fda75
hengstchon <hengstchon@gmail.com>
2020-08-11 13:37:32 +0200

    Add: scroll to top button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -18,6 +18,7 @@ export default ({ currentPage, totalPage, cat }) => {
       <Pagination
         count={totalPage}
         page={currentPage}
+        color="primary"
         renderItem={item => (
           <PaginationItem
             component={Link}
