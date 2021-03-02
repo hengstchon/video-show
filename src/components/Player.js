b4a82ffe12dd84d15cfde80c68632584e3688061
hengstchon <hengstchon@gmail.com>
2021-03-02 09:19:13 +0100

    add: buttons to jump to player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,27 +1,51 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import { makeStyles } from "@material-ui/core/styles"
-import ReactHlsPlayer from "react-hls-player"
-import { getUrl } from "../utils"
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import ReactHlsPlayer from 'react-hls-player'
+import { getUrl } from '../utils'
 
 const useStyles = makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
   player: {
-    maxHeight: `85vh`
-  }
+    maxHeight: `85vh`,
+    marginTop: `20px`,
+  },
+  link: {
+    fontSize: `20px`,
+    color: `inherit`,
+    marginRight: `10px`,
+    textDecoration: `none`,
+  },
 }))
 
 export default () => {
   const { vid } = useParams()
-  const url = getUrl(vid)
+  const { url, oriUrl } = getUrl(vid)
   const classes = useStyles()
-  const videoInfos = JSON.parse(localStorage.getItem("videoInfos"))
+  const videoInfos = JSON.parse(localStorage.getItem('videoInfos'))
   const title = videoInfos.find(i => i.vid === parseInt(vid)).title
 
   return (
     <>
       <div className={classes.toolbar} />
       <h2>{title}</h2>
+      <div>
+        <a className={classes.link} href={`mpv://${oriUrl}`}>
+          mpv
+        </a>
+        <a className={classes.link} href={`nplayer-${oriUrl}`}>
+          nplayer
+        </a>
+        <a
+          className={classes.link}
+          href={`intent:${oriUrl}#Intent;package=com.mxtech.videoplayer.pro;S.title=${title};end`}
+        >
+          mxplayer
+        </a>
+        <a className={classes.link} href={`vlc://${oriUrl}`}>
+          vlc
+        </a>
+      </div>
       <ReactHlsPlayer
         url={url}
         autoplay={false}
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,17 @@
-import axios from "axios"
-import JSSoup from "jssoup"
+import axios from 'axios'
+import JSSoup from 'jssoup'
 
-const CORS_PROXY = "https://evening-ridge-14001.herokuapp.com/"
+const CORS_PROXY = 'https://evening-ridge-14001.herokuapp.com/'
 
 // const ipInt = () => Math.floor(Math.random() * 255 + 1)
 const getHeaders = () => ({
-  "Accept-Language": "zh-CN,zh;q=0.9"
+  'Accept-Language': 'zh-CN,zh;q=0.9',
   // "X-Forwarded-For": `${ipInt()}.${ipInt()}.${ipInt()}.${ipInt()}`
 })
 
 const strencode = (input, key) => {
   input = atob(input)
-  let code = ""
+  let code = ''
   for (let i = 0; i < input.length; i++) {
     const k = i % key.length
     code += String.fromCharCode(input.charCodeAt(i) ^ key.charCodeAt(k))
@@ -21,10 +21,10 @@ const strencode = (input, key) => {
 
 const getInfo = soup => {
   let t = soup
-    .getText("|")
-    .replace(/&nbsp;/gi, " ")
-    .split("|")
-  if (t.length > 14) t = t.slice(1)
+    .getText('|')
+    .replace(/&nbsp;/gi, ' ')
+    .split('|')
+  if (t.length > 16) t = t.slice(1)
 
   const duration = t[0].trim()
   const title = t[1].trim()
@@ -34,8 +34,8 @@ const getInfo = soup => {
   const favorites = t[9].trim()
   const comments = t[11].trim()
   const points = t[13].trim()
-  const imgsrc = soup.find("img").attrs.src
-  const vhref = soup.find("a").attrs.href
+  const imgsrc = soup.find('img').attrs.src
+  const vhref = soup.find('a').attrs.href
   const vid = getVid(imgsrc)
 
   return {
@@ -49,20 +49,20 @@ const getInfo = soup => {
     duration,
     imgsrc,
     vhref,
-    vid
+    vid,
   }
 }
 
 const getVid = imgsrc => {
-  const last = imgsrc.split("/").pop()
-  const vid = last.split(".")[0]
+  const last = imgsrc.split('/').pop()
+  const vid = last.split('.')[0]
   return parseInt(vid)
 }
 
 export const getUrl = vid => {
   // const url = `https://ccn.91p52.com//m3u8/${vid}/${vid}.m3u8`
   const url = `https://cdn.91p07.com//m3u8/${vid}/${vid}.m3u8`
-  return CORS_PROXY + url
+  return { url: CORS_PROXY + url, oriUrl: url }
 }
 
 const getHtml = async url => {
@@ -75,9 +75,9 @@ const getHtml = async url => {
 // in video index page
 export const getInfos = async url => {
   const html = await getHtml(url)
-  localStorage.setItem("pageHtml", JSON.stringify(html))
+  localStorage.setItem('pageHtml', JSON.stringify(html))
   const soup = new JSSoup(html)
-  const listchannel = soup.findAll("div", "videos-text-align")
+  const listchannel = soup.findAll('div', 'videos-text-align')
   let infos = listchannel.map(i => getInfo(i))
   infos = infos.map((info, i) => ({ id: i + 1, ...info }))
   return infos
@@ -85,9 +85,9 @@ export const getInfos = async url => {
 
 // get total page of videos from like "当前最热 视频 1 - 24 的 60"
 export const getTotalPage = () => {
-  const html = JSON.parse(localStorage.getItem("pageHtml"))
+  const html = JSON.parse(localStorage.getItem('pageHtml'))
   const soup = new JSSoup(html)
-  const t = soup.find("div", "login_register_header").text.split(" ")
+  const t = soup.find('div', 'login_register_header').text.split(' ')
   const l = t.length
   const videoNum = parseInt(t[l - 1])
   const totalPage = Math.ceil(videoNum / 24)
@@ -103,7 +103,7 @@ const _getSrc = html => {
   } else {
     let src = /<source src="(.*?)"/.exec(html)
     src = src && src[1]
-    return src || "游客"
+    return src || '游客'
   }
 }
 
